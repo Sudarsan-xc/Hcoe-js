462965c55191ffe37ea1f5491a647264a07254b0
Sudarsan-xc <sudarsansharma2020@gmail.com>
2024-06-27 13:31:47 +0545

    basics done

---
+++ index.js
@@ -0,0 +1,23 @@
+//alert("my name is sudarsan")
+
+/*const person="sudrsan"
+console.log(person);
+const age = prompt("how old are you?");
+console.log(Number(age));
+const isValid = confirm("Are you above 18");
+console.log(isValid);
+*/
+//wap top calculate the simple interest and alert interst
+/*const p=prompt("what is the principle:");
+console.log(Number(p));
+const r=prompt("what is the rate:");
+console.log(Number(r));
+const t=prompt("what is the time:");
+console.log(Number(t));
+const si=prompt(p*t*r)/100;
+console.log(Number(si));
+alert(si);
+*/
+//type conversion
+/*const num= String(10)
+const num=Number(num);*/
